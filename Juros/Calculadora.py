ec2a92f1a770f0578a2d41201bd696c5cd76546f
Gabriel Purper Andrade e Silva <104388283+GabrielPurper@users.noreply.github.com>
2026-02-23 17:02:03 -0300

    Add files via upload

---
+++ Juros/Calculadora.py
@@ -0,0 +1,10 @@
+C = int(input("Digita o Capital Inicial:"))
+D = float(input("Digita o Desconto:"))
+T = int(input("Digita o Tempo:"))
+M =  C * (1 + D) ** T
+
+print("O valor do Juros Compostos é:", M)
+
+
+
+
