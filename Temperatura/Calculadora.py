64d8b6c31b697342d71149046180600791061f55
Gabriel Purper Andrade e Silva <104388283+GabrielPurper@users.noreply.github.com>
2026-02-23 17:08:31 -0300

    Add files via upload

---
+++ Temperatura/Calculadora.py
@@ -0,0 +1,11 @@
+F = float(input("Digite o Grau em Farhenheit:"))
+C = (F - 32) * (5 / 9)
+
+print("Temperatura em Farhenheit:", F)
+print("Temperatura em Celcius:", C)
+
+C = float(input("Graus em Celcius:"))
+F = ( 9 / 5 ) * C + 32; 
+ 
+print("Temperatura em Celcius:", C)
+print("Temperatura em Farhenheit:", F)
