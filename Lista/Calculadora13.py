8efe1aa1671d14043a5e8ce852964749f30e7ee6
Gabriel Purper Andrade e Silva <104388283+GabrielPurper@users.noreply.github.com>
2026-06-10 19:25:11 -0300

    Delete Lista directory

--- Lista/Calculadora13.py
+++
@@ -1,8 +0,0 @@
-notas = [("Fulano",7.5),("João",5.5),("André", 6.5), ("Carlos", 9.5), ("Washington", 8.5)]
-for i in range(0, len(notas)):
-        #l[1][1]
-        tupla = notas[i]
-        nome_aluno_tupla = tupla[0]
-        nota_tupla = tupla[1]
-        print(nota_tupla)
-
