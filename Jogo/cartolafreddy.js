f69d1a31756bd983106d913efb699bea82078ea3
Gabriel Purper Andrade e Silva <104388283+GabrielPurper@users.noreply.github.com>
2026-06-10 19:24:54 -0300

    Delete Jogo directory

--- Jogo/cartolafreddy.js
+++
@@ -1,8 +0,0 @@
-//Freddy
-function cartolafreddy(){
-    // Aumento de velocidade permanente de 5%.
-    player.speed = player.baseSpeed * 1.05;
-    console.log("Item: Cartola");
-    console.log("Efeito: Velocidade aumentada em 5%");
-    console.log("Nova velocidade: " + player.speed);
-}
--- Jogo/dropEndo.js
+++
@@ -1,8 +0,0 @@
-// Drop da moeda pelo Endoesqueleto
-function dropMoeda(x, y) {
-    const novaMoeda = document.createElement('div');
-    novaMoeda.className = 'moeda';
-    novaMoeda.style.left = x + 'px';
-    novaMoeda.style.top = y + 'px';
-    document.body.appendChild(novaMoeda);
-}
