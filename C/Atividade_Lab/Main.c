1c2083dffa35443dd1b2f31f3ca592b672d4f354
Gabriel Purper Andrade e Silva <104388283+GabrielPurper@users.noreply.github.com>
2026-08-27 13:44:19 -0300

    Add files via upload

---
+++ C/Atividade_Lab/Main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "calculadora.h"
+
+int main() {
+    float a = 10.0, b = 5.0;
+    printf("Soma: %.2f\n", somar(a, b));
+    printf("Subtração: %.2f\n", subtrair(a, b));
+    printf("Multiplicação: %.2f\n", multiplicar(a, b));
+    float somar(float a, float b);
+if (eh_divisao_valida(b)) {
+printf("Divisao: %.2f\n", dividir(a, b));
+} else {
+ printf("Erro: Divisao por zero!\n");
+}
+    return 0;
+}
