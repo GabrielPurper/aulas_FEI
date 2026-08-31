f0c14184138a46e86a84b021767351e23de14da0
Gabriel Purper Andrade e Silva <104388283+GabrielPurper@users.noreply.github.com>
2026-08-31 19:17:09 -0300

    Add files via upload

---
+++ Assemble/AssembleAtividade/atividade1semafaro.asm
@@ -0,0 +1,23 @@
+ORG 0000H
+
+INICIO:
+
+    MOV P1, 0FEH
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+
+    MOV P1, 0FDH
+    NOP
+    NOP
+
+    MOV P1, 0FBH
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+
+    SJMP INICIO   
