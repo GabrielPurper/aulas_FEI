dc07fab45f8dc96f5f32a37594b434f92143806d
Gabriel Purper Andrade e Silva <104388283+GabrielPurper@users.noreply.github.com>
2026-08-31 16:54:08 -0300

    Add files via upload

---
+++ Assemble/Atividade.asm
@@ -0,0 +1,38 @@
+ORG 0000H
+
+inicio:
+  
+    CLR P1.0       
+    SETB P1.1       
+    SETB P1.2      
+    
+    SETB P1.4      
+    CLR P1.5       
+
+   
+    JNB P2.0, B_ativo
+    SJMP inicio     
+
+B_ativo:
+   
+    SETB P1.0      
+    CLR P1.1       
+    NOP
+    NOP
+    NOP
+
+  
+    SETB P1.1      
+    CLR P1.2       
+    
+    SETB P1.5      
+    CLR P1.4       
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+
+    SJMP inicio      ;
+
+END
